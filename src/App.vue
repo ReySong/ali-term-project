970051d5d18feeeb615231d2797ae11aeac45bf9
Zen_Song <zensong001203@gmail.com>
2021-11-19 12:32:12 +0800

    feat: add fade-button component

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,87 @@
 <template>
   <div id="root" />
   <canvas ref="canvas" id="canvas" width="1000" height="1000"></canvas>
-  <button class="btn btn-drop"> 坠落 </button>
+  <button class="btn btn-drop">坠落</button>
   <parabolic-ball class="btn btn-parabola" />
   <!-- <button class="btn btn-parabola"> 抛物 </button> -->
-  <button class="btn btn-fade"> 淡入 </button>
-  <button class="btn btn-start"> 开始游戏！ </button>
+  <!-- <button class="btn btn-fade">淡入</button> -->
+  <fade-button />
+  <button class="btn btn-start">开始游戏！</button>
 </template>
 
 <script>
-import { FadeIn, FadeOut } from './scripts/fade/fade';
-import { KeyCode, KeyCodeMapTable } from './scripts/keycode.js';
-import { GameController, util, Button, buttons } from './scripts/drop/index.js';
-import ParabolicBall from './components/ParabolicBall';
-// import GenerateRandomLocElems from './scripts/generate';
+import { FadeIn, FadeOut } from './scripts/fade/fade'
+import { KeyCode, KeyCodeMapTable } from './scripts/keycode'
+import { GameController, util, Button, buttons } from './scripts/drop/index'
+import ParabolicBall from './components/ParabolicBall'
+import { Sounds } from './sounds/index'
+import GenerateRandomLocElems from './scripts/fade/generate'
+import FadeButton from './components/FadeButton.vue'
 
 export default {
   name: 'App',
   components: {
-    ParabolicBall
+    ParabolicBall,
+    FadeButton,
   },
   mounted() {
-    
-    let elem;
-    let id = "fadeExample"; 
-    let dropBtn = document.getElementsByClassName("btn-drop")[0];
-    let parabolaBtn = document.getElementsByClassName("btn-parabola")[0];
-    let fadeBtn = document.getElementsByClassName("btn-fade")[0];
+    let elem
+    let sound = new Sounds()
+    sound.playBackgroundAudio()
+    const dropBtn = document.getElementsByClassName('btn-drop')[0]
+    const parabolaBtn = document.getElementsByClassName('btn-parabola')[0]
+    const fadeBtn = document.getElementsByClassName('btn-fade')[0]
+    const startBtn = document.getElementsByClassName('btn-start')[0]
 
-    dropBtn.addEventListener("click", () => {
-      /**  
+    dropBtn.addEventListener('click', () => {
+      /**
        * drop样例展示
        */
-      const canvas = document.querySelector('#canvas');
-      const ctx = canvas.getContext('2d');
+      const canvas = document.querySelector('#canvas')
+      const ctx = canvas.getContext('2d')
       // 定义游戏相关的配置项
       const option = {
         // 绘图函数
         draw() {
-          util.background(canvas, ctx, 'white');
-          buttons.forEach(button => {
-            button.drop(ctx);
-            button.update();
-          });
+          util.background(canvas, ctx, 'white')
+          buttons.forEach((button) => {
+            button.drop(ctx)
+            button.update()
+          })
         },
         // 监听事件
         events: {
           // 键盘按下事件
           keyPressed(key) {
             //按键按下时触发一次，命中距离底部最近的按键
-            let targets = buttons.filter(button => button.key === key);
+            let targets = buttons.filter((button) => button.key === key)
             if (targets.length != 0) {
-              targets.sort((a, b) => b.position.y - a.position.y);
-              targets[0].target();
+              targets.sort((a, b) => b.position.y - a.position.y)
+              targets[0].target()
             }
           },
           // 用点击注册事件模拟按键产生过程
           mousePressed() {
-            const position = util.createVector(310, 180);
-            if (position.y > 0.8 * window.innerHeight) return;
-            const button = new Button(`${KeyCodeMapTable.get(KeyCode[Math.floor(Math.random() * KeyCode.length)])}`, position);
-            buttons.push(button);
+            const position = util.createVector(310, 180)
+            if (position.y > 0.8 * window.innerHeight) return
+            const button = new Button(
+              `${KeyCodeMapTable.get(KeyCode[Math.floor(Math.random() * KeyCode.length)])}`,
+              position
+            )
+            buttons.push(button)
           },
         },
-      };
+      }
       // 初始化游戏控制器
-      let gameController = new GameController(canvas, ctx, option);
+      let gameController = new GameController(canvas, ctx, option)
       // 游戏装载
-      gameController.setup();
-    });
+      gameController.setup()
+    })
 
-    parabolaBtn.addEventListener("click", () => {
+    parabolaBtn.addEventListener('click', () => {})
 
-    });
-
-    fadeBtn.addEventListener("click", () => {
-      /**  
+    fadeBtn.addEventListener('click', () => {
+      /**
        * fade样例展示
        */
       elem = {
@@ -83,19 +89,54 @@ export default {
         right: 22.3,
         width: 120,
         height: 60,
-        keycode: KeyCode[Math.floor(Math.random() * KeyCode.length)]
+        keycode: KeyCode[Math.floor(Math.random() * KeyCode.length)],
       }
-      let dom = document.getElementById(id);
-      if (dom) document.getElementById("root").removeChild(dom);
-      FadeIn(elem, id, 0);
+      let dom = document.getElementById('fadeExample')
+      if (dom) document.getElementById('root').removeChild(dom)
+      FadeIn(elem, 'fadeExample', 0)
       document.addEventListener('keyup', (event) => {
         if (event.keyCode === elem.keycode) {
-          FadeOut(id, 1);
+          FadeOut('fadeExample', 1)
         }
       })
-    });
-    
-  }
+    })
+
+    let id = '1'
+    let idList = new Map()
+    startBtn.addEventListener('click', () => {
+      dropBtn.style.display = 'none'
+      parabolaBtn.style.display = 'none'
+      fadeBtn.style.display = 'none'
+      let si = setInterval(() => {
+        // let effect = Math.floor(Math.random() * 3)
+        let randomElem = GenerateRandomLocElems()
+
+        // switch (effect) {
+        //   case 0:
+        //   case 1:
+        //   case 2:
+        idList.set(randomElem.keycode, id)
+        FadeIn(randomElem, id++)
+        document.addEventListener('keyup', (event) => {
+          let idExist = idList.get(event.keyCode)
+          if (idExist) {
+            idList.delete(idExist)
+            sound.playSuccess()
+            FadeOut(idExist, 1)
+          }
+        })
+
+        // }
+      }, 2000)
+      let flag = false
+      if (/* 音乐播放完 */ flag) {
+        clearInterval(si)
+        dropBtn.style.display = 'block'
+        parabolaBtn.style.display = 'block'
+        fadeBtn.style.display = 'block'
+      }
+    })
+  },
 }
 </script>
 
@@ -119,7 +160,7 @@ export default {
   border-radius: 15px 225px 255px 15px 15px 255px 225px 15px;
   border-style: solid;
   border-width: 2px;
-  box-shadow: rgba(0, 0, 0, .2) 15px 28px 25px -18px;
+  box-shadow: rgba(0, 0, 0, 0.2) 15px 28px 25px -18px;
   box-sizing: border-box;
   color: #41403e;
   cursor: pointer;
@@ -128,7 +169,7 @@ export default {
   font-size: 1rem;
   line-height: 23px;
   outline: none;
-  padding: .75rem;
+  padding: 0.75rem;
   text-decoration: none;
   transition: all 235ms ease-in-out;
   border-bottom-left-radius: 15px 255px;
@@ -141,12 +182,12 @@ export default {
 }
 
 .btn:hover {
-  box-shadow: rgba(0, 0, 0, .3) 2px 8px 8px -5px;
+  box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 8px -5px;
   transform: translate3d(0, 2px, 0);
 }
 
 .btn:focus {
-  box-shadow: rgba(0, 0, 0, .3) 2px 8px 4px -6px;
+  box-shadow: rgba(0, 0, 0, 0.3) 2px 8px 4px -6px;
 }
 
 .btn-start {
@@ -173,6 +214,4 @@ export default {
   right: 20%;
   bottom: 30%;
 }
-
 </style>
-
--- src/sounds/index.js
+++ src/sounds/index.js
@@ -1,37 +1,30 @@
 // todo 我不知道 vue 的脚手架怎么配置来支持静态资源文件，所以暂时用这种方法
-// const soundsFileURI =
-//  "https://github.com/Zen-Song/Zen-Song.Github.io/blob/feature/add-sounds/src/";
+const soundsFileURI = 'https://github.com/Zen-Song/Zen-Song.Github.io/blob/master/src/'
 
 export default class Sounds {
   constructor() {
     // initialize and preload sounds
-    this.successAudio = new Audio(
-        "./sounds/success.mp3?raw=true"
-    );
-    this.failureAudio = new Audio(
-        "./sounds/failure.mp3?raw=true"
-    );
-    this.doneAudio = new Audio( "./sounds/done.mp3?raw=true");
-    this.backgroundAudio = new Audio(
-        "./sounds/background.mp3?raw=true"
-    );
+    this.successAudio = new Audio(soundsFileURI + 'sounds/success.mp3?raw=true')
+    this.failureAudio = new Audio(soundsFileURI + './sounds/failure.mp3?raw=true')
+    this.doneAudio = new Audio(soundsFileURI + './sounds/done.mp3?raw=true')
+    this.backgroundAudio = new Audio(soundsFileURI + './sounds/background.mp3?raw=true')
   }
 
   playSuccess() {
-    this.successAudio.play();
+    this.successAudio.play()
   }
 
   playFailure() {
-    this.failureAudio.play();
+    this.failureAudio.play()
   }
 
   playDone() {
-    this.doneAudio.play();
+    this.doneAudio.play()
   }
 
   playBackgroundAudio() {
-    this.backgroundAudio.play();
+    this.backgroundAudio.play()
   }
 }
 
-export { Sounds };
+export { Sounds }
